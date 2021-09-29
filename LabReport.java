c1d9d566f5e7df7441cfb9aa8130f67e618e9742
Isha Shakya <ishashakya8@gmail.com>
2021-09-29 15:31:19 +0545

    committed

---
+++ LabReport.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labreport;
+
+/**
+ *
+ * @author ishas
+ */
+public class LabReport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        new BorderLayoutDemo();
+//        new BtnPractical("demo");
+//          new CheckboxPractical();
+//        new ChoicePractical();
+//        new MouseEventPractical("executed")
+//        DialogBoxDemo  dialogControlDemo = new DialogBoxDemo();
+//        dialogControlDemo.showDialogDemo();
+//        FileDialogDemo fileDialogDemo = new FileDialogDemo();
+//        GridBagDemo gridLayoutDemo = new GridBagDemo();
+//        gridLayoutDemo.showGridBagLayoutDemo();
+//        innerClassDemo innerClassDemo = new innerClassDemo("Demo");
+//        InnerClassEventHandling a = new InnerClassEventHandling();
+//        new JbuttonPractical(); 
+//        new JComboBoxDemo();
+//        new JScrollPaneDemo();
+//        new JTabbedPaneDemo();
+//        new JTableDemo();
+//        new MenuPractical();
+//        new MouseEventHandling();
+        new PizzaDelivery();
+//        new TextEventHandling();
+
+    }
+    
+}
